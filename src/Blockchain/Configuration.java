9d677f15e084012541d00a196eb0255d325bc967
The Loraxxz <tilmann.lorenz@mail.com>
2022-02-11 18:11:33 +0100

    Added first functions -> still trying to understand blockchain

--- src/Blockchain/Configuration.java
+++ src/Blockchain/Configuration.java
@@ -1,7 +1,12 @@
 package Blockchain;
 
+import java.util.ArrayList;
+
 public enum Configuration {
     instance;
     int difficulty = 3;
     int transactionSequence = 0;
+    Transaction genesisTransaction;
+    float minimumTransaction = 0.1f;
+    ArrayList<Block> blockchain = new ArrayList<>();
 }
